6ef166bea7bb2a5f0d9251b504b7cbfb902ee68e
Jomir Uddin <juddin@nyc.yearup.org>
2019-11-18 16:53:59 -0500

    javascript complete

--- script.js
+++ script.js
@@ -1,8 +1,53 @@
-for (let i = ; i <= 3; i++) {
-    const url = `https://fizal.me/pokeapi/api/v2/id/${i}.json`
+const pokedex = document.getElementById("pokedex");
+
+console.log(pokedex)
+
+const fetchPokemon = () => {
+
+    const promises = [];
+    for( let i = 1; i <= 802; i++){
+        const url = `http://pokeapi.co/api/v2/pokemon/${i}`;
+        promises.push(fetch(url).then((res) => res.json()));
+    }
+
+    Promise.all(promises).then( results => {
+        const pokemon = results.map( data => ({
+            name: data.name,
+            id: data.id,
+            image: data.sprites['front_default'],
+            type: data.types.map( type => type.type.name).join(", ")
+        }));
+        displayPokemon(pokemon);
+    });
+};
+
+const displayPokemon = (pokemon) => {
+    console.log(pokemon);
+    const pokemonHTMLString = pokemon
+        .map(
+            (pokeman) => `
+        <li class="card">
+            <img class="card-image" src="${pokeman.image}"/>
+            <h2 class="card-title">${pokeman.id}. ${pokeman.name}</h2>
+            <p class="card-subtitle">Type: ${pokeman.type}</p>
+        </li>
+    `
+        )
+        .join('');
+    pokedex.innerHTML = pokemonHTMLString;
+};
+
+fetchPokemon();
+
+
+/*
+for (let i = 1; i <= 3; i++) {
+    const url = `https://fizal.me/pokeapi/api/v2/id/${i}.json
     fetch(url)
-        .then(res => res.json())
+        .then(res => res.json()
         .then(pokemon => {
             console.log(pokemon);
         });
 }
+*/
+
